66f8ddd148871c1d10616dd82da14898badcb80b
nick <nikhilnick00007@gmail.com>
2019-04-17 22:35:23 +0900

    rolex: Ros-fy implement vendorsetup.sh

--- revengeos_rolex.mk
+++ revengeos_rolex.mk
@@ -20,8 +20,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
-# Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common RevengeOs stuff
+$(call inherit-product, vendor/revengeos/config/common.mk)
+$(call inherit-product, vendor/revengeos/config/gsm.mk)
+
+# Inherit some common aosp stuff
+TARGET_GAPPS_ARCH := arm64
+IS_PHONE := true
+TARGET_BOOT_ANIMATION_RES := 720
+
+#Make Build OFFICIAL
+REVENGEOS_BUILDTYPE := OFFICIAL
 
 # Inherit from rolex device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -30,7 +39,7 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := rolex
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi 4A
-PRODUCT_NAME := lineage_rolex
+PRODUCT_NAME := revengeos_rolex
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
@@ -41,4 +50,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="rolex" \
     PRIVATE_BUILD_DESC="rolex-user 7.1.2 N2G47H V9.2.6.0.NCCMIEK release-keys"
 
-BUILD_FINGERPRINT := Xiaomi/rolex/rolex:7.1.2/N2G47H/V9.2.6.0.NCCMIEK:user/release-keys
+    BUILD_FINGERPRINT := Xiaomi/rolex/rolex:7.1.2/N2G47H/V9.2.6.0.NCCMIEK:user/release-keys
